6ec67d57b18662ab32f3407b232b39b0fab33028
ramsysproject <m.ramirez05@gmail.com>
2019-08-15 17:00:16 -0300

    Initial project setup

---
+++ src/main/java/com/emramirez/strcalc/StrcalcApplication.java
@@ -0,0 +1,13 @@
+package com.emramirez.strcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrcalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrcalcApplication.class, args);
+	}
+
+}
